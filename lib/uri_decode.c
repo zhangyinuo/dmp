01300294c7277f5ee26b30ce01dac99d0fab9043
jingchun.zhang <jingchun.zhang@code.(none)>
2015-02-05 20:47:11 +0800

    msg

---
+++ lib/uri_decode.c
@@ -0,0 +1,161 @@
+#include "uri_decode.h"
+
+void ngx_unescape_uri(u_char **dst, u_char **src, size_t size, int type)
+{
+    u_char  *d, *s, ch, c, decoded;
+    enum {
+        sw_usual = 0,
+        sw_quoted,
+        sw_quoted_second
+    } state;
+
+    d = *dst;
+    s = *src;
+
+    state = 0;
+    decoded = 0;
+
+    while (size--) {
+
+        ch = *s++;
+
+        switch (state) {
+        case sw_usual:
+            if (ch == '?'
+                && (type & (NGX_UNESCAPE_URI|NGX_UNESCAPE_REDIRECT)))
+            {
+                *d++ = ch;
+                goto done;
+            }
+
+            if (ch == '%') {
+                state = sw_quoted;
+                break;
+            }
+
+            *d++ = ch;
+            break;
+
+        case sw_quoted:
+
+            if (ch >= '0' && ch <= '9') {
+                decoded = (u_char) (ch - '0');
+                state = sw_quoted_second;
+                break;
+            }
+
+            c = (u_char) (ch | 0x20);
+            if (c >= 'a' && c <= 'f') {
+                decoded = (u_char) (c - 'a' + 10);
+                state = sw_quoted_second;
+                break;
+            }
+
+            /* the invalid quoted character */
+
+            state = sw_usual;
+
+            *d++ = ch;
+
+            break;
+
+        case sw_quoted_second:
+
+            state = sw_usual;
+
+            if (ch >= '0' && ch <= '9') {
+                ch = (u_char) ((decoded << 4) + ch - '0');
+
+                if (type & NGX_UNESCAPE_REDIRECT) {
+                    if (ch > '%' && ch < 0x7f) {
+                        *d++ = ch;
+                        break;
+                    }
+
+                    *d++ = '%'; *d++ = *(s - 2); *d++ = *(s - 1);
+
+                    break;
+                }
+
+                *d++ = ch;
+
+                break;
+            }
+
+            c = (u_char) (ch | 0x20);
+            if (c >= 'a' && c <= 'f') {
+                ch = (u_char) ((decoded << 4) + c - 'a' + 10);
+
+                if (type & NGX_UNESCAPE_URI) {
+                    if (ch == '?') {
+                        *d++ = ch;
+                        goto done;
+                    }
+
+                    *d++ = ch;
+                    break;
+                }
+
+                if (type & NGX_UNESCAPE_REDIRECT) {
+                    if (ch == '?') {
+                        *d++ = ch;
+                        goto done;
+                    }
+
+                    if (ch > '%' && ch < 0x7f) {
+                        *d++ = ch;
+                        break;
+                    }
+
+                    *d++ = '%'; *d++ = *(s - 2); *d++ = *(s - 1);
+                    break;
+                }
+
+                *d++ = ch;
+
+                break;
+            }
+
+            /* the invalid quoted character */
+
+            break;
+        }
+    }
+
+done:
+
+    *dst = d;
+    *src = s;
+}
+
+/*
+int main(int c, char **v)
+{
+	if (c != 2)
+	{
+		fprintf(stderr, "Usage %s uri_encode_str!\n", basename(v[0]));
+		return -1;
+	}
+
+	char dst[1024] = {0x0};
+
+	u_char * sdst = dst;
+
+	u_char * ssrc = v[1];
+
+	ngx_unescape_uri(&sdst, &ssrc, strlen(v[1]), 0);
+
+	fprintf(stdout, "%s\n", dst);
+
+	memset(dst, 0, sizeof(dst));
+
+	ssrc = v[1];
+	sdst = dst;
+
+	ngx_unescape_uri(&sdst, &ssrc, strlen(v[1]), 1);
+
+	fprintf(stdout, "%s\n", dst);
+
+	return 0;
+}
+*/
---
+++ network/http_client/vfs_data_base.c
@@ -0,0 +1,110 @@
+/*
+* Copyright (C) 2012-2014 www.56.com email: jingchun.zhang AT renren-inc.com; jczhang AT 126.com ; danezhang77 AT gmail.com
+* 
+* 56VFS may be copied only under the terms of the GNU General Public License V3
+*/
+
+/*
+ *base文件，查询基础配置信息，设置相关基础状态及其它```
+ *Tracker 数目较少，放在一个静态数组
+ *CS和FCS数目较多，放在hash链表
+ *CS FCS ip信息采用uint32_t 存储，便于存储和查找
+ */
+volatile extern int maintain ;		//1-维护配置 0 -可以使用
+extern t_vfs_up_proxy g_proxy;
+
+static inline int isDigit(const char *ptr) 
+{
+	return isdigit(*(unsigned char *)ptr);
+}
+
+static int active_connect(char *ip, int port)
+{
+	int fd = createsocket(ip, port);
+	if (fd < 0)
+	{
+		LOG(vfs_sig_log, LOG_ERROR, "connect %s:%d err %m\n", ip, port);
+		return -1;
+	}
+	if (svc_initconn(fd))
+	{
+		LOG(vfs_sig_log, LOG_ERROR, "svc_initconn err %m\n");
+		close(fd);
+		return -1;
+	}
+	add_fd_2_efd(fd);
+	LOG(vfs_sig_log, LOG_NORMAL, "fd [%d] connect %s:%d\n", fd, ip, port);
+	return fd;
+}
+
+static void create_header(char *httpheader, t_task_base *base, t_task_sub *sub, int len)
+{
+	char *t = base->url + 7;
+
+	char *e = strchr(t, '/');
+	int l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", t);
+
+	if (e)
+	{
+		l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", e+1);
+		*e = 0x0;
+		l = snprintf(httpheader + l, len - l, "Host: %s\r\n\r\n", t); 
+		*e = '/';
+	}
+	else
+	{
+		l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", t);
+		l = snprintf(httpheader + l, len - l, "Host: %s\r\n\r\n", sub->peerip); 
+	}
+}
+
+void check_task()
+{
+	t_vfs_tasklist *task = NULL;
+	int ret = 0;
+	while (1)
+	{
+		ret = vfs_get_task(&task, g_queue_tmp);
+		if (ret != GET_TASK_OK)
+		{
+			LOG(vfs_sig_log, LOG_TRACE, "vfs_get_task get notihng %d\n", ret);
+			break;
+		}
+		vfs_set_task(task, g_queue);
+	}
+
+	uint16_t once_run = 0;
+	while (1)
+	{
+		once_run++;
+		if (once_run >= g_config.cs_max_task_run_once)
+			return;
+		ret = vfs_get_task(&task, g_queue);
+		if (ret != GET_TASK_OK)
+		{
+			LOG(vfs_sig_log, LOG_TRACE, "vfs_get_task get notihng %s\n", task_status[g_queue]);
+			return ;
+		}
+		t_task_base *base = &(task->task.base);
+		t_task_sub *sub = &(task->task.sub);
+		LOG(vfs_sig_log, LOG_DEBUG, "vfs_get_task get from %s %s\n", task_status[g_queue], base->filename);
+		int fd = active_connect(sub->peerip, sub->peerport);
+		if (fd < 0)
+		{
+			LOG(vfs_sig_log, LOG_ERROR, "connect %s %d error %m\n", sub->peerip, sub->peerport);
+			base->overstatus = OVER_TOO_MANY_TRY;
+			vfs_set_task(task, TASK_FIN);  
+			continue;
+		}
+		sub->lastlen = 0;
+		char httpheader[1024] = {0x0};
+		create_header(httpheader, base, sub, sizeof(httpheader));
+		active_send(fd, httpheader);
+		struct conn *curcon = &acon[fd];
+		vfs_cs_peer *peer = (vfs_cs_peer *) curcon->user;
+		peer->recvtask = task;
+		peer->sock_stat = RECV_HEAD_ING;
+	}
+}
+
+
