8cb45301623ca31178c210f6d907abe2d992a4bd
jingchun.zhang <jingchun.zhang@code.(none)>
2015-02-06 09:49:03 +0800

    msg

--- test/test_scws.c
+++ test/test_scws.c
@@ -6,15 +6,15 @@ void main()
 {
 	scws_t s;
 	scws_res_t res, cur;
-	char *text = "澶у濂斤紝鎴戞槸涓浗浜猴紝鏉ヨ嚜瀹夊窘";
+	char *text = "绿盟信息安全好";
 
 	if (!(s = scws_new())) {
 		printf("ERROR: cann't init the scws!\n");
 		exit(-1);
 	}
-	scws_set_charset(s, "utf8");
-	scws_set_dict(s, "./dict.utf8.xdb", SCWS_XDICT_XDB);
-	scws_set_rule(s, "/ott/install/scws/etc/rules.utf8.ini");
+	scws_set_charset(s, "gbk");
+	scws_set_dict(s, "./full.xdb", SCWS_XDICT_XDB);
+	scws_set_rule(s, "/ott/install/scws/etc/rules.gbk.ini");
 
 	scws_send_text(s, text, strlen(text));
 	while (res = cur = scws_get_result(s))
