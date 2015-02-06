f6ba83a37dbcd497c7918e1e5d8e87296560f2c1
jingchun.zhang <jingchun.zhang@code.(none)>
2015-02-06 18:14:14 +0800

    msg

--- network/http_client/vfs_data_base.c
+++ network/http_client/vfs_data_base.c
@@ -37,74 +37,15 @@ static int active_connect(char *ip, int port)
 	return fd;
 }
 
-static void create_header(char *httpheader, t_task_base *base, t_task_sub *sub, int len)
+static void create_header(char *domain, char *url, char *httpheader)
 {
-	char *t = base->url + 7;
+	int l = sprintf(httpheader, "GET /%s HTTP/1.1\r\n", url);
 
-	char *e = strchr(t, '/');
-	int l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", t);
-
-	if (e)
-	{
-		l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", e+1);
-		*e = 0x0;
-		l = snprintf(httpheader + l, len - l, "Host: %s\r\n\r\n", t); 
-		*e = '/';
-	}
-	else
-	{
-		l = snprintf(httpheader, len, "GET /%s HTTP/1.1\r\n", t);
-		l = snprintf(httpheader + l, len - l, "Host: %s\r\n\r\n", sub->peerip); 
-	}
+	l += sprintf(httpheader + l, "Host: %s\r\n\r\n", domain);
 }
 
 void check_task()
 {
-	t_vfs_tasklist *task = NULL;
-	int ret = 0;
-	while (1)
-	{
-		ret = vfs_get_task(&task, g_queue_tmp);
-		if (ret != GET_TASK_OK)
-		{
-			LOG(vfs_sig_log, LOG_TRACE, "vfs_get_task get notihng %d\n", ret);
-			break;
-		}
-		vfs_set_task(task, g_queue);
-	}
-
-	uint16_t once_run = 0;
-	while (1)
-	{
-		once_run++;
-		if (once_run >= g_config.cs_max_task_run_once)
-			return;
-		ret = vfs_get_task(&task, g_queue);
-		if (ret != GET_TASK_OK)
-		{
-			LOG(vfs_sig_log, LOG_TRACE, "vfs_get_task get notihng %s\n", task_status[g_queue]);
-			return ;
-		}
-		t_task_base *base = &(task->task.base);
-		t_task_sub *sub = &(task->task.sub);
-		LOG(vfs_sig_log, LOG_DEBUG, "vfs_get_task get from %s %s\n", task_status[g_queue], base->filename);
-		int fd = active_connect(sub->peerip, sub->peerport);
-		if (fd < 0)
-		{
-			LOG(vfs_sig_log, LOG_ERROR, "connect %s %d error %m\n", sub->peerip, sub->peerport);
-			base->overstatus = OVER_TOO_MANY_TRY;
-			vfs_set_task(task, TASK_FIN);  
-			continue;
-		}
-		sub->lastlen = 0;
-		char httpheader[1024] = {0x0};
-		create_header(httpheader, base, sub, sizeof(httpheader));
-		active_send(fd, httpheader);
-		struct conn *curcon = &acon[fd];
-		vfs_cs_peer *peer = (vfs_cs_peer *) curcon->user;
-		peer->recvtask = task;
-		peer->sock_stat = RECV_HEAD_ING;
-	}
 }
 
 
